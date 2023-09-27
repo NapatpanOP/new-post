c95caef11c675824217212e931d11ab5c2a49f49
NapatpanOP <napatpanns424@gmail.com>
2023-09-27 16:55:48 +0700

    Edit the date and time.

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function boardPosts() {
           </div>
           <div>
             <h3 class="font-semibold text-gray-600">${data.title}</h3>
-            <div class="font-light text-sm mb-2 text-gray-500">${data.time}</div>
+            <div class="font-light text-sm mb-2 text-gray-500">${data.date}</div>
             <div class="font-light">${data.message}</div>
           </div>
         </div>`
