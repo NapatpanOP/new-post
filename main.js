cf5b6704eb2f622c41573d6472e0a75ff3efc7a0
NapatpanOP <napatpanns424@gmail.com>
2023-09-27 21:22:45 +0700

    Add a post edit page.

--- main.js
+++ main.js
@@ -16,14 +16,19 @@ function boardPosts() {
       datas.forEach(data => {
         // console.log(data);
         newPosts.innerHTML += `
-        <div class="flex gap-16 mt-8">
+        <div class="flex gap-12 mt-8">
           <div class="font-light">
             ${data.id}
           </div>
-          <div>
-            <h3 class="font-semibold text-gray-600">${data.title}</h3>
-            <div class="font-light text-sm mb-2 text-gray-500">${data.date}</div>
-            <div class="font-light">${data.message}</div>
+          <div class="flex content-between gap-8 max-w-5xl w-full">
+            <div class="w-full">
+              <h3 class="font-semibold text-gray-600">${data.title}</h3>
+              <div class="font-light text-sm mb-2 text-gray-500">${data.date}</div>
+              <div class="font-light">${data.message}</div>
+            </div>
+            <div>
+              <a href="./edit-post.html" class="font-light text-xs text-gray-400 underline hover:text-gray-500">Edit</a>
+            </div>
           </div>
         </div>`
       });
