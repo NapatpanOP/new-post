755e3f2790e884f1854d9c104eacf4a4d0d3c705
NapatpanOP <napatpanns424@gmail.com>
2023-10-03 11:26:27 +0700

    Edit variable name

--- topic.js
+++ topic.js
@@ -1,67 +1,64 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const urlParams = new URLSearchParams(window.location.search);
-  const postId = urlParams.get('id');
-  const title = document.getElementById('title');
-  const name = document.getElementById('name');
-  const date = document.getElementById('date');
-  const message = document.getElementById('message');
-  const comments = document.getElementById('comments');
-  const commentInput = document.getElementById('commentInput');
+const urlParams = new URLSearchParams(window.location.search);
+const postId = urlParams.get('id');
+const title = document.getElementById('title');
+const personName = document.getElementById('name');
+const date = document.getElementById('date');
+const message = document.getElementById('message');
+const comments = document.getElementById('comments');
+const commentInput = document.getElementById('commentInput');
 
-  console.log(postId);
+// console.log(postId);
   
-  const config = {
-    headers: {
-      'Accept': 'application/json',
-    },
-  }
+const config = {
+  headers: {
+    'Accept': 'application/json',
+  },
+}
 
-  fetch(`http://localhost:3000/posts/${postId}`, config)
-      .then(response => response.json())
-      .then(data => {
-        title.innerHTML = data.title;
-        name.innerHTML = data.personName;
-        date.innerHTML = data.date;
-        message.innerHTML = data.message;
-      });
-
-    fetch(`http://localhost:3000/comments?postId=${postId}`, config)
+fetch(`http://localhost:3000/posts/${postId}`, config)
     .then(response => response.json())
     .then(data => {
-      data.forEach(comment => {
-        comments.innerHTML += `
-          <div class="font-light mb-4">
-            ${comment.comment}
-          </div>`;
-      });
+      title.innerHTML = data.title;
+      personName.innerHTML = data.personName;
+      date.innerHTML = data.date;
+      message.innerHTML = data.message;
     });
 
-  commentInput.addEventListener('keyup', (e) => {
-    if (e.key === 'Enter') {
-      const comment = commentInput.value;
-
-      if (comment.trim() !== '') {
-        sendCommentToServer(comment, postId);
-        commentInput.value = '';
-      }
-    }
+fetch(`http://localhost:3000/comments?postId=${postId}`, config)
+.then(response => response.json())
+.then(data => {
+  data.forEach(comment => {
+    comments.innerHTML += `
+      <div class="font-light mb-4">
+        ${comment.comment}
+      </div>`;
   });
+});
 
-  function sendCommentToServer(comment, postId) {
-
-    const data = {
-      postId: postId,
-      comment: comment,
-    };
-  
-    fetch('http://localhost:3000/comments', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    })
-
-    window.location.reload()
+commentInput.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    const comment = commentInput.value;
+    
+    if (comment.trim() !== '') {
+      sendComment(comment, postId);
+      commentInput.value = '';
+    }
   }
-})
+});
+
+function sendComment(comment, postId) {
+  const data = {
+    postId: postId,
+    comment: comment,
+  };
+
+  fetch('http://localhost:3000/comments', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+  
+  window.location.reload()
+}
